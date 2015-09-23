9f1ab75433cc34aaacafcfa7abf1bd6782723cfe
GestionPro <rigovidal92@gmail.com>
2015-09-22 20:48:47 -0400

    ADMINISTRACION DE USUARIOS

--- GestionPro/usuario/urls.py
+++ GestionPro/usuario/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from . import views
+from django.contrib.auth.decorators import login_required
 urlpatterns = patterns('',
     # Examples:
     url(r'^index/$', 'usuario.views.inicio', name='Inicio'),
@@ -14,7 +15,8 @@ urlpatterns = patterns('',
     url(r'^usuario/logout/$', 'usuario.views.cerrar_sesion', name='Cerrar Sesion'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^usuario/new/$', 'usuario.views.nuevo_user'),
-    url(r'^usuario/lista/$', views.UsuarioList.as_view(), name='usuario_lista'),
-    url(r'^modificar/(?P<pk>\d+)$', views.UpdateUser.as_view(), name='modificar_usuario'),
+    url(r'^usuario/lista/$', login_required(views.UsuarioList.as_view()), name='usuario_lista'),
+    url(r'^modificar/(?P<pk>\d+)$', login_required(views.UpdateUser.as_view()), name='modificar_usuario'),
+    url(r'^cambiar_clave/(?P<pk>\d+)$', login_required('usuario.views.cambio_clave'), name='cambio_clave'),
 
 )
--- usuario/views.py
+++ usuario/views.py
@@ -7,17 +7,15 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from models import Usuario
-from forms import FormUsuario, FormUsuarioChange, UpdatePasswordForm
+from forms import FormUsuario, FormUsuarioChange, AdminPasswordChangeForm
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 
 
 def inicio(request):
-    return render_to_response('base.html',context_instance=RequestContext(request))
+    return render_to_response('base.html', context_instance=RequestContext(request))
 
-def nuevo_usuario(request):
-    return render_to_response('usuarios/nuevo_usuario.html',context_instance=RequestContext(request))
 
 @login_required(login_url='/usuario/login')
 @permission_required('auth.add_user', login_url='/usuario/login')
@@ -26,10 +24,11 @@ def nuevo_user(request):
         formulario = FormUsuario(request.POST)
         if formulario.is_valid():
             formulario.save()
-            return redirect('usuario_lista')
+            return HttpResponseRedirect('/usuario/lista/')
     else:
         formulario = FormUsuario()
-        return render_to_response('usuarios/nuevo_usuario.html', {'formulario': formulario}, context_instance=RequestContext(request))
+        return render_to_response('usuarios/nuevo_usuario.html', {'formulario': formulario},
+                                  context_instance=RequestContext(request))
 
 
 def iniciar_sesion(request):
@@ -40,7 +39,7 @@ def iniciar_sesion(request):
         if formulario.is_valid:
             username = request.POST['username']
             clave = request.POST['password']
-            acceso = authenticate(username = username, password = clave)
+            acceso = authenticate(username=username, password=clave)
             if acceso is not None:
                 if acceso.is_active:
                     login(request, acceso)
@@ -52,7 +51,8 @@ def iniciar_sesion(request):
     else:
         formulario = AuthenticationForm()
 
-    return  render_to_response('usuarios/login.html', {'formulario': formulario}, context_instance=RequestContext(request))
+    return render_to_response('usuarios/login.html', {'formulario': formulario},
+                              context_instance=RequestContext(request))
 
 
 @login_required(login_url='/usuario/login')
@@ -66,8 +66,13 @@ class UsuarioList(ListView):
     template_name = 'usuarios/lista_usuario.html'
 
 
-class UpdateUser(UpdateView):
+class UsuarioDelete(DeleteView):
+    model = Usuario
+    template_name = 'usuarios/borrar_usuario.html'
+    success_url = '/usuario/lista/'
 
+
+class UpdateUser(UpdateView):
     template_name = 'usuarios/modificar_usuario.html'
     model = Usuario
     form_class = FormUsuarioChange
@@ -78,12 +83,14 @@ class UpdateUser(UpdateView):
         return super(UpdateUser, self).dispatch(*args, **kwargs)
 
 
+@login_required(login_url='/usuario/login')
 def cambio_clave(request):
     if request.method == 'POST':
-        formulario = UpdatePasswordForm(request.POST)
-        if formulario.is_valid():
+        formulario = AdminPasswordChangeForm(request.POST, request.user)
+        if formulario.is_valid:
             formulario.save()
             return redirect('/index/')
     else:
-        formulario = UpdatePasswordForm()
-        return render_to_response('usuarios/cambiar_clave.html', {'formulario': formulario}, context_instance=RequestContext(request))
+        formulario = AdminPasswordChangeForm(request.POST)
+        return render_to_response('usuarios/cambiar_clave.html', {'formulario': formulario},
+                                  context_instance=RequestContext(request))
